a1f10adacef2816355582a0f27a9e680e1535325
hi1004 <kipo09@naver.com>
2023-11-04 00:32:42 +0900

    feat: conect firebase sdk

---
+++ src/firebaseApp.ts
@@ -0,0 +1,29 @@
+import { FirebaseApp, getApp, initializeApp } from 'firebase/app';
+
+export let app: FirebaseApp;
+const {
+  VITE_FIREBASE_API_KEY,
+  VITE_FIREBASE_AUTH_DOMAIN,
+  VITE_FIREBASE_PROJECT_ID,
+  VITE_FIREBASE_STORAGE_BUCKET,
+  VITE_FIREBASE_APP_ID,
+  VITE_FIREBASE_MEASUREMENT_ID,
+} = import.meta.env;
+
+const firebaseConfig = {
+  apiKey: VITE_FIREBASE_API_KEY,
+  authDomain: VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: VITE_FIREBASE_PROJECT_ID,
+  storageBucket: VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: VITE_FIREBASE_APP_ID,
+  appId: VITE_FIREBASE_MEASUREMENT_ID,
+};
+
+try {
+  app = getApp('app');
+} catch (error) {
+  app = initializeApp(firebaseConfig, 'app');
+}
+
+export const firebase = initializeApp(firebaseConfig);
+export default firebase;
